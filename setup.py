8b217f1f772a6b3b665a46d8f0589dc97b67c1b1
Joeri Abbo <joeriabbo@hotmail.com>
2023-11-28 14:10:55 +0100

    Improve client

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ with open('requirements.txt') as f:
     required = f.read().splitlines()
 
 setup(
-    name='pip-package-tracefy-clients',
-    version='0.0.0',
+    name='pip_package_tracefy_clients',
+    version='0.0.1',
     packages=find_packages(),
     install_requires=required
 )
