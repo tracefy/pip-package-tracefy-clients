fbeab12dccedd174d296f7b8070b135b7e35bd94
Joeri Abbo <joeriabbo@hotmail.com>
2023-12-12 14:45:32 +0100

    Add SQS

--- TracefyClients/__init__.py
+++ TracefyClients/__init__.py
@@ -2,3 +2,4 @@ from .sql_client import SQLClient
 from .flask_client import FlaskClient
 from .dynamo_db_client import DynamoDBClient
 from .redis_client import RedisClient
+from .sqs_client import SQSClient
