c0db77d7d2618767c411b3ddcfca358c613ce2d1
Joeri Abbo <joeriabbo@hotmail.com>
2023-12-12 13:19:03 +0100

    Add redis

--- TracefyClients/__init__.py
+++ TracefyClients/__init__.py
@@ -1,3 +1,4 @@
 from .sql_client import SQLClient
 from .flask_client import FlaskClient
 from .dynamo_db_client import DynamoDBClient
+from .redis_client import RedisClient
---
+++ TracefyClients/redis_client.py
@@ -0,0 +1,33 @@
+import os
+from dotenv import load_dotenv
+import redis
+
+load_dotenv()
+
+
+class RedisClient:
+
+    def __init__(self):
+        self.client = redis.StrictRedis(
+            self.get_host(),
+            self.get_port(),
+            username=self.get_username(),
+            password=self.get_password(),
+            charset="utf-8",
+            decode_responses=True
+        )
+
+    def get_host(self) -> str:
+        return os.getenv("REDIS_STREAM_ADDRESS", "localhost")
+
+    def get_port(self) -> int:
+        return int(os.getenv("REDIS_STREAM_PORT", "6379"))
+
+    def get_username(self) -> str:
+        return os.getenv("REDIS_STREAM_USERNAME", "")
+
+    def get_password(self) -> str:
+        return os.getenv("REDIS_STREAM_PASSWORD", "")
+
+    def get_client(self):
+        return self.client
