d161728c7b49fca84b5c02566eb4b673b8b3f3d4
Joeri Abbo <joeriabbo@hotmail.com>
2023-12-22 12:02:04 +0100

    Add s3 client.

--- TracefyClients/__init__.py
+++ TracefyClients/__init__.py
@@ -3,5 +3,5 @@ from .flask_client import FlaskClient
 from .dynamo_db_client import DynamoDBClient
 from .redis_client import RedisClient
 from .sqs_client import SQSClient
-from .s3_client import SQSClient
+from .s3_client import S3Client
 from .slack_client import SlackClient
